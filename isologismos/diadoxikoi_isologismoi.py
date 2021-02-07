a5c8b09b35edd174680bb1688793af74f3d53484
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-07 17:29:18 +0200

    Update diadoxikoi_isologismoi.py

--- isologismos/diadoxikoi_isologismoi.py
+++ isologismos/diadoxikoi_isologismoi.py
@@ -13,7 +13,7 @@ while apo < 0:
 arx = float(input("Δώστε ποσό αρχικού κεφαλαίου: "))
 tel = float(input("Δώστε ποσό τελικού κεφαλαίου: "))
 x = tel - arx - eis + apo
-if arx < 0 or tel:
+if arx < 0 or tel < 0:
     print ("Το αποτέλεσμα είναι:", x)
 elif x > 0:
     print ("Το κέρδος είναι:", x)
