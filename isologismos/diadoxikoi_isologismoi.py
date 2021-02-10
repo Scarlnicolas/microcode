5a1a7b58cadd63ef4fea753edda25028e7fc39ae
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-10 18:09:19 +0200

    Update diadoxikoi_isologismoi.py

--- isologismos/diadoxikoi_isologismoi.py
+++ isologismos/diadoxikoi_isologismoi.py
@@ -8,16 +8,14 @@ while eis < 0:
     eis = float(input("Δώστε έγκυρο ποσό εισφορών: "))
 apo = float(input("Δώστε ποσό απολήψεων: "))
 while apo < 0:
-    print ("Ο αριθμός είναι αρνητικός.")
+    print ("Το ποσό είναι αρνητικό.")
     apo = float(input("Δώστε έγκυρο ποσό απολήψεων: "))
 arx = float(input("Δώστε ποσό αρχικού κεφαλαίου: "))
 tel = float(input("Δώστε ποσό τελικού κεφαλαίου: "))
 x = tel - arx - eis + apo
-if arx < 0 or tel < 0:
+if arx <= 0 or tel <= 0:
     print ("Το αποτέλεσμα είναι:", x)
 elif x > 0:
     print ("Το κέρδος είναι:", x)
-elif x < 0:
-    print ("Η ζημιά είναι:", x)
 else:
-    print ("Το κέρδος είναι:", x, "|", "Η ζημιά είναι:", x)
+    print ("Η ζημιά είναι:", x)
