4e9ea7b3b14021c551f3e00a3d4477b5b5e953a2
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-11 22:31:38 +0200

    Update anatokismos.py

--- oikonomika_mathimatika/anatokismos.py
+++ oikonomika_mathimatika/anatokismos.py
@@ -5,5 +5,5 @@
 present_value = float(input("Αρχικό κεφάλαιο: "))
 i = float(input("Επιτόκιο (π.χ: 0.06 αντί για 6%): "))
 n = int(input("Χρονικά διαστήματα: "))
-compount_amount = present_value * (1 + i)**n
+compount_amount = present_value * (1 + i) ** n
 print ("Τελικό κεφάλαιο:", compount_amount, "ευρώ")
