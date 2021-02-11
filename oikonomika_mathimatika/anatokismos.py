26b7f833de8eac637100073dc03df904567d1ad4
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-11 22:21:47 +0200

    Create anatokismos.py

---
+++ oikonomika_mathimatika/anatokismos.py
@@ -0,0 +1,9 @@
+# Python 8.3.5
+# Εύρεση τελικού κεφαλαίου.
+# Σύνθετος τόκος ή ανατοκισμός. 
+
+present_value = float(input("Αρχικό κεφάλαιο: "))
+i = float(input("Επιτόκιο (π.χ: 0.06 αντί για 6%): "))
+n = int(input("Χρονικά διαστήματα: "))
+compount_amount = present_value * (1 + i)**n
+print ("Τελικό κεφάλαιο:", compount_amount, "ευρώ")
