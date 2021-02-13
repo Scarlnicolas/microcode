2ec6f7871af0ab4f909c733b6c78523796aebbf9
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-13 08:46:15 +0200

    Update Daily salary to hourly pay equivalent_GR.py

--- Salary calculator/Daily salary to hourly pay equivalent_GR.py
+++ Salary calculator/Daily salary to hourly pay equivalent_GR.py
@@ -1,6 +1,6 @@
 # Python 3.8.5
 # Μετατροπή ημερομισθίου (6 ωρών και 40 λεπτών) σε ωρομίσθιο.
 
-daily_wage = float(input("Δώστε ποσό ημερομισθίου: "))
-hourly_wage = daily_wage * 0.15
-print ("Το ωρομίσθιο είναι", hourly_wage, "ευρώ")
+daily_salary = float(input("Δώστε ποσό ημερομισθίου: "))
+hourly_salary = daily_salary * 0.15
+print ("Το ωρομίσθιο είναι", hourly_salary, "ευρώ")
