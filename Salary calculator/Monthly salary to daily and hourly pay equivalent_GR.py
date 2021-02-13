a8275f1bc2c6f01f617a7a8a06d176b570649408
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-13 08:45:22 +0200

    Update Monthly salary to daily and hourly pay equivalent_GR.py

--- Salary calculator/Monthly salary to daily and hourly pay equivalent_GR.py
+++ Salary calculator/Monthly salary to daily and hourly pay equivalent_GR.py
@@ -1,10 +1,10 @@
 # Python 3.8.5
 # Μετατροπή μισθού (40 ωρών εβδομαδιαίας εργασίας) σε ωρομίσθιο και σε ημερομίσθιο.
 
-salary = float(input("Δώστε ποσό μισθού: "))
-hourly_salary = salary * 0.006
+monthly_salary = float(input("Δώστε ποσό μισθού: "))
+hourly_salary = monthly_salary * 0.006
 print ("Το ωρομίσθιο είναι", hourly_salary, "ευρώ")
-daily_salary_8 = salary * 0.048
+daily_salary_8 = monthly_salary * 0.048
 print ("Το ημερομίσθιο 8 ωρών είναι", daily_salary_8, "ευρώ")
-daily_salary_6_40 = salary / 25
+daily_salary_6_40 = monthly_salary / 25
 print ("Το ημερομίσθιο 6 ωρών και 40 λεπτών είναι", daily_salary_6_40, "ευρώ")
