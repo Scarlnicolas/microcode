10a07474912b308baa30868566b592e5696c9f62
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-13 02:21:23 +0200

    Update teliko_kefalaio.py

--- anatokismos/teliko_kefalaio.py
+++ anatokismos/teliko_kefalaio.py
@@ -4,9 +4,9 @@
 
 present_value = float(input("Αρχικό κεφάλαιο: "))
 # Παράδειγμα: επιτόκιο 0.06 αντί για 6%.
-epi = float(input("Επιτόκιο: ")) 
+interest_rate = float(input("Επιτόκιο: ")) 
 # Παράδειγμα 1: 5 έτη και 1 εξάμηνο --> 5 * 2 + 1 = 11 εξάμηνα --> Χρονικά διαστήματα: 11.
 # Παράδειγμα 2: 4 έτη --> Χρονικά διαστήματα: 4.
-chron = int(input("Χρονικά διαστήματα: ")) 
-tel_Kef = present_value * (1 + epi) ** chron
-print ("Τελικό κεφάλαιο:", tel_Kef)
+time = int(input("Χρονικά διαστήματα: ")) 
+capital = present_value * (1 + interest_rate) ** time
+print ("Τελικό κεφάλαιο:", capital)
