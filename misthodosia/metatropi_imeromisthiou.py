799347d7da4303234b6d71314718c3df7d2001c9
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-13 02:43:39 +0200

    Update metatropi_imeromisthiou.py

--- misthodosia/metatropi_imeromisthiou.py
+++ misthodosia/metatropi_imeromisthiou.py
@@ -1,6 +1,6 @@
 # Python 3.8.5
 # Μετατροπή ημερομισθίου (6 ωρών και 40 λεπτών) σε ωρομίσθιο.
 
-day_wage = float(input("Δώστε ποσό ημερομισθίου: "))
-hourly_wages = day_wage * 0.15
+daily_wage = float(input("Δώστε ποσό ημερομισθίου: "))
+hourly_wages = daily_wage * 0.15
 print ("Το ωρομίσθιο είναι", hourly_wages, "ευρώ")
