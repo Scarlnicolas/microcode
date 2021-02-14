d1c05e1854fd3e70cddb46b5c2efc1302d704531
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-14 07:10:23 +0200

    Create monthly interest_GR.py

---
+++ Simple interest/monthly interest_GR.py
@@ -0,0 +1,8 @@
+# Python 8.3.5
+# Εύρεση τόκου σε μήνες. 
+
+capital = float(input("Κεφάλαιο: "))
+months = int(input("Μήνες: "))
+interest_rate = float(input("Επιτόκιο: "))
+interest = capital * months * interest_rate / 12
+print ("Ο τόκος είναι:", interest, "ευρώ")
