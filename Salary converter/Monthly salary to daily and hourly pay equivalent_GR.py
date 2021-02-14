82bd4843f55f776c25bda11210e31d49a513cba4
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-14 14:19:47 +0200

    Update Monthly salary to daily and hourly pay equivalent_GR.py

--- Salary converter/Monthly salary to daily and hourly pay equivalent_GR.py
+++ Salary converter/Monthly salary to daily and hourly pay equivalent_GR.py
@@ -3,8 +3,8 @@
 
 monthly_salary = float(input("Δώστε ποσό μισθού: "))
 hourly_salary = monthly_salary * 0.006
-print ("Το ωρομίσθιο είναι", hourly_salary, "ευρώ")
+print ("Το ωρομίσθιο είναι", round(hourly_salary, 2), "ευρώ")
 daily_salary_8 = monthly_salary * 0.048
-print ("Το ημερομίσθιο 8 ωρών είναι", daily_salary_8, "ευρώ")
+print ("Το ημερομίσθιο 8 ωρών είναι", round(daily_salary_8, 2), "ευρώ")
 daily_salary_6_40 = monthly_salary / 25
-print ("Το ημερομίσθιο 6 ωρών και 40 λεπτών είναι", daily_salary_6_40, "ευρώ")
+print ("Το ημερομίσθιο 6 ωρών και 40 λεπτών είναι", round(daily_salary_6_40, 2), "ευρώ")
