5290924fb3af5e93eb884aa0ed112f52a4005a76
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-14 14:05:08 +0200

    Update Daily interest_GR.py

--- Simple interest/Daily interest_GR.py
+++ Simple interest/Daily interest_GR.py
@@ -11,4 +11,4 @@ while days_in_year != 360 and days_in_year != 365 and days_in_year != 366:
 "\n" + "Δώστε 360 ή 365 ή 366 ημέρες.")
     days_in_year = int(input("Ημέρες του έτους: "))
 interest = capital * days * interest_rate / days_in_year
-print ("Ο τόκος είναι:", interest)
+print ("Ο τόκος είναι:", round(interest,2))
