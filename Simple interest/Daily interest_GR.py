768b96bcfd40c2bb8b8942129e7ed117b2395072
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-14 08:22:58 +0200

    Update Daily interest_GR.py

--- Simple interest/Daily interest_GR.py
+++ Simple interest/Daily interest_GR.py
@@ -5,10 +5,10 @@
 capital = float(input("Κεφάλαιο: "))
 days = int(input("Τοκοφόφες Ημέρες: "))
 interest_rate = float(input("Επιτόκιο: "))
-yearly_days = int(input("Ημέρες του έτους: "))
+days_in_year = int(input("Ημέρες του έτους: "))
 while yearly_days != 360 and yearly_days != 365 and yearly_days != 366:
     print ("Προσπαθήστε ξανά!"
 "\n" + "Δώστε 360 ή 365 ή 366 ημέρες.")
-    yearly_days = int(input("Ημέρες του έτους: "))
-interest = capital * days * interest_rate / yearly_days
+    days_in_year = int(input("Ημέρες του έτους: "))
+interest = capital * days * interest_rate / days_in_year
 print ("Ο τόκος είναι:", interest)
