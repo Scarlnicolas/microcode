a2a3baa7f727082e6423b3ab8fcb6c886c8dad5b
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2021-02-14 09:00:30 +0200

    Update Daily interest_GR.py

--- Simple interest/Daily interest_GR.py
+++ Simple interest/Daily interest_GR.py
@@ -11,9 +11,7 @@ while days_in_year != 360 and days_in_year != 365 and days_in_year != 366:
 "\n" + "Δώστε 360 ή 365 ή 366 ημέρες.")
     days_in_year = int(input("Ημέρες του έτους: "))
 interest = capital * days * interest_rate / days_in_year
-if days_in_year == 365:
+if days_in_year == 365 or days_in_year == 366:
     print ("Ο τόκος με χρήση το πολιτικό έτος είναι:", interest)
-elif days_in_year == 366:
-    print ("Ο τόκος για με χρήση το δίσεκτο πολιτικό έτος είναι", interest)
 else:
     print("Ο τόκος με χρήση το μεικτό ή εμπορικό έτος είναι:", interest)
