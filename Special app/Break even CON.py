b44bde88ca235177dd9c312c7e67120449070af4
Scarlnicolas <77108476+Scarlnicolas@users.noreply.github.com>
2022-11-04 21:56:58 +0200

    Update Break even CON.py

--- Special app/Break even CON.py
+++ Special app/Break even CON.py
@@ -1,17 +1,17 @@
 ice = input("do you know ice number c? ")
 if ice == "yes":
-    print("ok!")
+    print ("ok!")
 ice_c = float(input("give an ice number c: "))
 price = float(input("give a price number: " ))
 variable = input("do you know variable_c? ")
 if variable == "yes":
-    print("ok")
+    print ("ok")
     variable_c = float(input("give a viariable c: "))
 elif variable == "no":
-    print("you don't know variable c. ")
+    print ("you don't know variable c. ")
     quan = input("do you know quantility? ")
     if quan == "yes":
-        print("ok")
+        print ("ok")
         quan_value = float(input("give a quantility number: "))
         if quan_value == quan_value < 0 or quan_value > 0 or quan_value == 0:
             vone = input ("do you know variable one? ")
@@ -23,4 +23,4 @@ elif variable == "no":
                 else:
                     print ("failed") 
 break_even = ice_c / price - variable_c
-print("break one:" , break_even)
+print ("break one:" , break_even)
